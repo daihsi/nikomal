f15038eb9a8d648b562e8c368cd31eb02abe4923
daishi <isitomo.mm@gmail.com>
2020-07-30 00:13:21 +0000

    Fix: ユーザー画像のサイズ修正

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,7 +1,7 @@
 @if (count($users) > 0)
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-11">
+<div class="container">
+    <div class="row justify-content-center mt-3">
+        <div class="col-12">
             <div class="card-group mx-auto">
                 <div class="row" id="user_list">
                     @foreach ($users as $user)
@@ -9,9 +9,9 @@
                             <div class="card-body">
                             <a href="{{ route('users.show', ['user' => $user->id]) }}">
                                 @empty($user->avatar)
-                                    <img src="{{ asset('storage/images/default_icon.png') }}" class="user_avatar_icon rounded-circle" width="170" height="170">
+                                    <img src="{{ asset('storage/images/default_icon.png') }}" class="user_avatar_icon rounded-circle" width="155" height="155">
                                 @else
-                                    <img src="{{ $user->avatar }}" class="user_avatar_icon rounded-circle" width="170" height="170">
+                                    <img src="{{ $user->avatar }}" class="user_avatar_icon rounded-circle" width="155" height="155">
                                 @endempty
                             </a>
                                 {!! link_to_route('users.show', $user->name, ['user' => $user->id], ['class' => 'btn btn-link text-dark font-weight-bold']) !!}
@@ -40,7 +40,7 @@
         <a href="{{ $users->nextPageUrl() }}" class="pagination_next"></a>
     </p>
     <p class="text-center mt-3">
-      <button class="view_more_button btn btn-success btn-lg active" aria-pressed="true">もっと見る</button>
+      <button class="view_more_button btn btn-success btn-lg" aria-pressed="true">もっと見る</button>
     </p>
 @endif
 @endif
