74ef2e8a8a91992977cd2386477262a7eb0364de
daishi <isitomo.mm@gmail.com>
2020-07-30 00:06:52 +0000

    Move: ゲストトップページコンテンツファイル移動

---
+++ resources/views/commons/guest_toppage.blade.php
@@ -0,0 +1,56 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8 col-md-10 col-10">
+             <div id="carouselExampleFade" class="carousel slide carousel-fade" data-intrebal=4000 data-touch=true data-ride="carousel">
+                <div class="carousel-inner shadow p-2 mb-5 bg-white rounded">
+                    <div class="carousel-item active">
+                        <img class="animalsCarousel" src="{{ asset('storage/images/smile4.jpg') }}" alt="イヌの笑顔"> 
+                    </div>
+                    <div class="carousel-item">
+                        <img class="animalsCarousel" src="{{ asset('storage/images/smile3.jpg') }}" alt="フクロウの笑顔">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="animalsCarousel" src="{{ asset('storage/images/smile7.jpg') }}" alt="イヌの笑顔">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="animalsCarousel" src="{{ asset('storage/images/smile8.jpg') }}" alt="ネコの笑顔">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="animalsCarousel" src="{{ asset('storage/images/smile1.jpg') }}" alt="ゾウの笑顔">
+                    </div>
+                </div>
+                     <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+            </div>
+        </div>
+    </div>
+</div>
+  
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h2 class="font-weight-bold mt-4 mb-4">
+                <p>さぁ、動物たちの笑っている表情を</p>
+                <p>投稿共有して一緒に癒されましょう</p>
+            </h2>
+            <a class="btn btn-success btn-lg mr-5" href="#">投稿する</a>
+            <a class="btn btn-outline-info btn-lg" href="#">はじめに</a>
+        </div>
+    </div>
+</div>
+    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="border mb-5 mt-5"></div>
+        </div>
+    </div>
+</div>
+
+<h3 class="text-center font-weight-bold mt-5 mb-4">みんなの投稿</h3>
