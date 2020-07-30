461a215dbfc784e591c6f3f43b65836625743093
daishi <isitomo.mm@gmail.com>
2020-07-30 00:40:18 +0000

    Move: ゲストトップページコンテンツファイル移動

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,64 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-    @guest
-        <div class="row justify-content-center">
-            <div class="col-6">
-                 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-intrebal=4000 data-touch=true data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="animalsCarousel" src="{{ asset('storage/images/smile4.jpg') }}" alt="イヌの笑顔"> 
-                        </div>
-                        <div class="carousel-item">
-                            <img class="animalsCarousel" src="{{ asset('storage/images/smile3.jpg') }}" alt="フクロウの笑顔">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="animalsCarousel" src="{{ asset('storage/images/smile7.jpg') }}" alt="イヌの笑顔">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="animalsCarousel" src="{{ asset('storage/images/smile8.jpg') }}" alt="ネコの笑顔">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="animalsCarousel" src="{{ asset('storage/images/smile1.jpg') }}" alt="ゾウの笑顔">
-                        </div>
-                    </div>
-                     <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-  
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h2 class="font-weight-bold mt-4 mb-4">
-                    <p>さぁ、動物たちの笑っている表情を</p>
-                    <p>投稿共有して一緒に癒されましょう</p>
-                </h2>
-                <a class="btn btn-success btn-lg mr-5" href="#">投稿する</a>
-                <a class="btn btn-outline-info btn-lg" href="#">はじめに</a>
-            </div>
-        </div>
-    </div>
-    
-    <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-11">
-                <div class="border mb-5 mt-5"></div>
-            </div>
-        </div>
-    </div>
-
-    <h3 class="text-center font-weight-bold mt-5 mb-4">みんなの投稿</h3>
-    @endguest
+@guest
+    @include('commons.guest_toppage')
+@endguest
 
     <div class="container-fluid">
         <div class="row justify-content-center">
@@ -87,5 +32,7 @@
             </div>
         </div>
     </div>
+    
+    @include('posts.posts')
 
 @endsection
