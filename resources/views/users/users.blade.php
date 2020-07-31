1faf7639bb241d52fa887a62dca1fe130b140f35
daishi <isitomo.mm@gmail.com>
2020-07-31 15:29:08 +0000

    Add: ユーザー一覧にフォロー・アンフォローボタン追加

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,38 +1,42 @@
 @if (count($users) > 0)
 <div class="container">
-    <div class="row justify-content-center mt-3">
-        <div class="col-12">
-            <div class="card-group mx-auto">
-                <div class="row" id="user_list">
-                    @foreach ($users as $user)
-                        <div class="card border-0 col-sm-6 col-md-4 col-xl-2 d-none d-sm-block"　style="width: 100px">
-                            <div class="card-body">
-                            <a href="{{ route('users.show', ['user' => $user->id]) }}">
-                                @empty($user->avatar)
-                                    <img src="{{ asset('storage/images/default_icon.png') }}" class="user_avatar_icon rounded-circle" width="155" height="155">
-                                @else
-                                    <img src="{{ $user->avatar }}" class="user_avatar_icon rounded-circle" width="155" height="155">
-                                @endempty
-                            </a>
-                                {!! link_to_route('users.show', $user->name, ['user' => $user->id], ['class' => 'btn btn-link text-dark font-weight-bold']) !!}
-                            </div>
+    <div class="row mt-5 mx-auto" id="user_list">
+        @foreach ($users as $user)
+            <div class="user_card col-6 col-sm-6 col-lg-4">
+                <div class="card border-0 d-none d-sm-block mb-5 text-center">
+                    <div class="d-flex justify-content-center">
+                        <a href="{{ route('users.show', ['user' => $user->id]) }}">
+                            @empty($user->avatar)
+                                <img src="{{ asset('storage/images/default_icon.png') }}" class="user_avatar_icon rounded-circle" width="155" height="155">
+                            @else
+                                <img src="{{ $user->avatar }}" class="user_avatar_icon rounded-circle" width="155" height="155">
+                            @endempty
+                        </a>
+                        <div class="d-flex flex-column ml-2 align-self-center">
+                            <a href="{{ route('users.show', $user->id) }}" class="btn btn-link text-dark font-weight-bold ">{{ $user->name }}</a>
+                            {{-- フォロー/アンフォローボタン--}}
+                            @include('user_follow.follow_button_users')
                         </div>
-                        <div class="card border-0 col-6 d-sm-none d-block">
-                            <div class="card-body">
-                            <a href="{{ route('users.show', ['user' => $user->id]) }}">
-                                @empty($user->avatar)
-                                    <img src="{{ asset('storage/images/default_icon.png') }}" class="user_avatar_icon rounded-circle" width="70" height="70">
-                                @else
-                                    <img src="{{ $user->avatar }}" class="user_avatar_icon rounded-circle" width="70" height="70">
-                                @endempty
-                            </a>
-                                {!! link_to_route('users.show', $user->name, ['user' => $user->id], ['class' => 'btn btn-link text-dark font-weight-bold']) !!}
-                            </div>
+                    </div>
+                </div>
+                <div class="card border-0 d-sm-none d-block mb-5 text-center">
+                    <div class="d-sm-flex justify-content-center">
+                        <a href="{{ route('users.show', ['user' => $user->id]) }}">
+                            @empty($user->avatar)
+                                <img src="{{ asset('storage/images/default_icon.png') }}" class="user_avatar_icon rounded-circle" width="85" height="85">
+                            @else
+                                <img src="{{ $user->avatar }}" class="user_avatar_icon rounded-circle" width="85" height="85">
+                            @endempty
+                        </a>
+                        <div class="d-sm-flex flex-column ml-2 align-self-center">
+                            <a href="{{ route('users.show', $user->id) }}" class="btn btn-link text-dark font-weight-bold mr-auto">{{ $user->name }}</a>
+                            {{-- フォロー/アンフォローボタン--}}
+                            @include('user_follow.follow_button_users')
                         </div>
-                    @endforeach
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 @if ($users->hasMorePages())
