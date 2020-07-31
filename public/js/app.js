c1afe51b5afd436095b4c655eddb6261448126c0
daishi <isitomo.mm@gmail.com>
2020-07-31 15:15:44 +0000

    Add: フォロー一覧・フォロワー一覧のスクロールコード追加

--- public/js/app.js
+++ public/js/app.js
@@ -47422,6 +47422,24 @@ var infScroll = new InfiniteScroll(user_list, {
   scrollThreshold: false,
   hideNav: '.pagination'
 });
+var following_list = document.getElementById('following_list');
+var infScroll = new InfiniteScroll(following_list, {
+  path: '.followings_pagination_next',
+  append: '.following_card',
+  history: false,
+  button: '.following_more_button',
+  scrollThreshold: false,
+  hideNav: '.pagination'
+});
+var follower_list = document.getElementById('follower_list');
+var infScroll = new InfiniteScroll(follower_list, {
+  path: '.follower_pagination_next',
+  append: '.following_card',
+  history: false,
+  button: '.follower_more_button',
+  scrollThreshold: false,
+  hideNav: '.pagination'
+});
 
 /***/ }),
 
