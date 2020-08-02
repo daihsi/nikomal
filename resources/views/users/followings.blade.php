f431e02b19bf5789efe950aa0eed6e655a195846
daishi <isitomo.mm@gmail.com>
2020-08-02 15:14:27 +0000

    Move: フォロー/アンフォロー関係のファイルを移動

---
+++ resources/views/users/followings.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+{{-- ユーザー情報 --}}
+@include('users.information')
+
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            {{-- タブ一覧 --}}
+            @include('commons.navtabs_user_show')
+            {{-- フォロー一覧 --}}
+            @include('users.users')
+            @if(count($users) == 0)
+                <div class="d-flex justify-content-center align-items-center" style="height:200px; color:rgba(0,0,0,0.4);">
+                    まだフォローしていません
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+
+@endsection
