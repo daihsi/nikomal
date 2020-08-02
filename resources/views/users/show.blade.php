3e026364b581ca71cf62175d6ce4c29a83815b5e
daishi <isitomo.mm@gmail.com>
2020-08-02 15:18:05 +0000

    Move: ユーザー詳細の情報を別ファイルに移動。部品化

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,54 +2,23 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row">
-        <div class="justify-content-lg-end d-none d-lg-flex col-lg-5">
-            @empty($user->avatar)
-                <img src="{{ asset('storage/images/default_icon.png') }}" class="user_show_avatar rounded-circle ml-5" width="230" height="230">
-            @else
-                <img src="{{ $user->avatar }}" class="rounded-circle ml-5 user_show_avatar" width="230" height="230">
-            @endempty
-        </div>
+{{-- ユーザー情報 --}}
+@include('users.information')
 
-        <div class="d-flex justify-content-center d-lg-none d-flex col-12">
-            @empty($user->avatar)
-                <img src="{{ asset('storage/images/default_icon.png') }}" class="user_show_avatar rounded-circle mr-3" width="230" height="230">
-            @else
-                <img src="{{ $user->avatar }}" class="rounded-circle mr-3 user_show_avatar" width="230" height="230">
-            @endempty
-            @if(Auth::id() === $user->id)
-              <div class="d-lg-none d-block align-self-end">{!! link_to_route('users.edit', '編集', ['user' => $user->id], ['class' => 'btn btn-outline-success rounded-pill fas fa-user-edit']) !!}</div>
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            {{-- タブ一覧 --}}
+            @include('commons.navtabs_user_show')
+            {{-- 自分の投稿一覧 --}}
+            @include('posts.posts')
+            @if(count($posts) == 0)
+                <div class="d-flex justify-content-center align-items-center" style="height:200px; color:rgba(0,0,0,0.4);">
+                    まだ投稿がありません
+                </div>
             @endif
-        </div>
-
-        @if(Auth::id() === $user->id)
-            <div class="flex-lg-column col-lg-6 pr-5 d-none d-lg-flex" style="max-width: 485px;">
-                <div class="align-self-end">{!! link_to_route('users.edit', '編集', ['user' => $user->id], ['class' => 'btn btn-outline-success rounded-pill fas fa-user-edit']) !!}</div>
-                <div class="font-weight-bold mt-3">{{ $user->name }}</div>
-                <div class="pt-1 overflow-auto" style="max-height: 150px;">
-                    <p>{!! nl2br(e($user->self_introduction)) !!}</p>
-                </div>
-            </div>
-        @else
-            <div class="flex-lg-column align-self-center col-lg-6 pr-5 d-none d-lg-flex" style="max-width: 485px;">
-                <div class="font-weight-bold mt-3">{{ $user->name }}</div>
-                <div class="overflow-auto mt-1" style="max-height: 150px;">
-                    <p>{!! nl2br(e($user->self_introduction)) !!}</p>
-                </div>
-              </div>
-        @endif
-    </div>
-    <div class="row justify-content-center d-lg-none d-flex">
-        <div class="col-12 flex-column ml-5" style="max-width: 485px;">
-            <div class="font-weight-bold">{{ $user->name }}</div>
-            <div class="pt-3 overflow-auto" style="max-height: 150px;">
-                <p>{!! nl2br(e($user->self_introduction)) !!}</p>
-            </div>
         </div>
     </div>
 </div>
-{{-- タブ一覧 --}}
-@include('commons.navtabs_user_show')
 
 @endsection
