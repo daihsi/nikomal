957e45312e73358a54d9ec26567e61df50334001
daishi <isitomo.mm@gmail.com>
2020-09-03 03:35:32 +0000

    Fix: 人気投稿の絞り込み条件変更

--- resources/views/commons/posts_sort_button.blade.php
+++ resources/views/commons/posts_sort_button.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-lg-9 col-sm-11 mt-3 mb-5 d-inline-flex">
+        <div class="col-lg-9 col-sm-11 mt-1 mb-4 d-inline-flex">
             @if (Request::is('/'))
                 <a href="{{ url('/') }}" class="btn btn-primary btn-block mr-4" role="button">新着投稿</a>
                 <a href="{{ route('posts.popular') }}" class="btn btn-outline-secondary btn-block posts_sort" role="button">人気投稿</a>
