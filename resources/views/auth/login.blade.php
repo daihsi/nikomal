0cb41123a08e82ab811e6f99d6443279d03f8b5a
daishi <isitomo.mm@gmail.com>
2020-09-06 15:44:19 +0000

    Add: 簡単ログイン実装

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,14 @@
 @section('content')
 <div class="container">
     <h2 class="text-center font-weight-bold mt-5 mb-5">{{ __('Login') }}</h2>
-    
-    <div class="row justify-content-center text-center">
-        <div class="col-10 col-sm-8 col-md-6 col-lg-5 col-xl-4">
-            <form method="POST" action="{{ route('login') }}">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-9 col-md-8 col-lg-6 col-xl-5">
 
+            <!-- 通常ログイン -->
+            <form method="POST" action="{{ route('login') }}" accept-charset="UTF-8">
                 @csrf
-
                 <div class="form-group">
-                    <label for="email" class="col col-form-label text-center">{{ __('E-Mail Address') }}</label>
+                    <label for="email" class="col col-form-label">{{ __('E-Mail Address') }}</label>
                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -19,9 +18,8 @@
                         </span>
                     @enderror
                  </div>
-
                 <div class="form-group">
-                    <label for="password" class="col col-form-label text-center">{{ __('Password') }}</label>
+                    <label for="password" class="col col-form-label">{{ __('Password') }}</label>
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                     @error('password')
                         <span class="invalid-feedback" role="alert">
@@ -29,8 +27,7 @@
                         </span>
                     @enderror
                 </div>
-
-                <div class="form-group mt-1">
+                <div class="form-group mt-2">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                         <label class="form-check-label" for="remember">
@@ -38,25 +35,36 @@
                         </label>
                     </div>
                 </div>
-
                 <div class="form-group mt-3 mb-4">
                     <button type="submit" class="btn btn-outline-success">
                         {{ __('Login') }}
                     </button>
                 </div>
+            </form>
 
-                <div class="form-group mb-2">
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-                </div>
-                
-                <div class="form-group">
-                    {!! link_to_route('register', 'ユーザー登録の方はこちらへ', [], ['class' => 'btn btn-link']) !!} 
+            <!-- ゲストユーザーログイン -->
+            <form method="POST" action="{{ route('login') }}" accept-charset="UTF-8">
+                @csrf
+                <input type="hidden" name="email" value="guest@example.com">
+                <input type="hidden" name="password" value="guest123456789">
+                <div class="form-group mb-3">
+                    <button type="submit" class="btn btn-warning">
+                        かんたんログイン
+                    </button>
                 </div>
             </form>
+
+            <!-- パスワード再設定・ユーザー登録ページリンク -->
+            <div class="mb-2">
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                @endif
+            </div>
+            <div>
+                {!! link_to_route('register', 'ユーザー登録の方はこちらへ', [], ['class' => 'btn btn-link']) !!}
+            </div>
         </div>
     </div>
 </div>
