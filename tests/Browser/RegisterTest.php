0b657d713f0769106f4ba9ccc306c8b4073baa6e
daishi <isitomo.mm@gmail.com>
2020-09-11 09:17:48 +0000

    ユーザー登録ブラウザテスト

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -3,13 +3,15 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use App\User;
 
 class RegisterTest extends DuskTestCase
 {
-    //use DatabaseMigrations;
+    use DatabaseMigrations;
     /**
      * A Dusk test example.
      *
@@ -21,19 +23,20 @@ class RegisterTest extends DuskTestCase
     {
         $password = '123456789';
         $user = factory(User::class)->make();
-        $this->browse(function (Browser $browser) {
+        $this->browse(function ($browser) use ($user, $password) {
             $browser->visit('/register')
-                    ->screenshot('Register')
-                    ->type('name', 'test1')
-                    ->type('email', 'test1@test1.com')
-                    ->type('password', '11111111')
-                    ->type('password_confirmation', '11111111')
-                    ->press('登録する');
-                    //->assertPathIs('/');
+                    ->type('name', $user->name)
+                    ->type('email', $user->email)
+                    ->type('password', $password)
+                    ->type('password_confirmation', $password)
+                    ->press('登録する')
+                    ->assertPathIs('/')
+                    ->assertSee('ユーザー登録完了しました') //toastrのフラッシュメッセージが表示されているか確認
+                    ->screenshot('register');
         });
     }
 
-    //バリデーションに引っ掛かった際のテスト
+    //バリデーションで登録ページにリダイレクトしたかテスト
     public function testValidationRegister()
     {
         //名前欄の入力が、一文字多い
@@ -46,10 +49,37 @@ class RegisterTest extends DuskTestCase
                     ->type('email', $user->email)
                     ->type('password', $password)
                     ->type('password_confirmation', $password)
-                    ->screenshot('test')
                     ->press('登録する')
+                    ->assertPathIs('/register')
+                    ->assertSee('ユーザー登録に失敗しました') //toastrのフラッシュメッセージが表示されているか確認
+                    ->screenshot('register');
+        });
+    }
+
+    //画像プレビューに画像が表示されているかテスト
+    public function testAvatarUpload()
+    {
+        Storage::fake('users_avatar');
+        $upload_file = UploadedFile::fake()->image('avatar_test.jpg')->size(2048);
+        $upload_file->move('storage/framework/testing/disks/users_avatar');
+        $file_name = $upload_file->getFilename();
+
+        $this->browse(function ($browser) use ($file_name) {
+            $browser->visit('/register')
+                    ->click('#avatarUploadButton')
+                    ->attach('#avatarUpload', 'storage/framework/testing/disks/users_avatar/'.$file_name)
+                    ->screenshot('users_avatar')
+                    ->assertSourceHas('image/'); //プレビュー用のソースコードに切り替わっているか確認
+        });
+    }
+
+    //ヘッダーナビゲーションから登録ページへ遷移しているかテスト
+    public function testLinkRegister()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('ユーザー登録')
                     ->assertPathIs('/register');
-                    //->dump();
         });
     }
 }
