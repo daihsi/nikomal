a2b04c933c60f83ec6351df17917e738f4177311
daishi <isitomo.mm@gmail.com>
2020-09-21 11:07:25 +0000

    Add: 投稿検索ブラウザテスト

---
+++ tests/Browser/PostSearchTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Post;
+use App\User;
+use App\PostImage;
+use App\Animal;
+
+class PostSearchTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+        $this->posts = factory(Post::class, 12)->create([
+                    'user_id' => $this->user->id,
+                    ])
+                    ->each(function ($post) {
+                        //画像データの生成
+                        $post->postImages()
+                            ->save(
+                                factory(PostImage::class)->make()
+                            );
+                        //動物カテゴリーデータの生成
+                        $post->postCategorys()
+                            ->createMany(
+                                factory(Animal::class, 3)->make()
+                                ->toArray()
+                            );
+                    });
+    }
+
+    //投稿検索テスト
+    public function testPostSearch()
+    {
+        $post10 = $this->posts[11];
+        $post9 = $this->posts[10];
+        $post8 = $this->posts[9];
+
+        $this->browse(function ($first, $second, $third) use ($post10, $post9, $post8) {
+
+            //キーワードのみ値を入れて検索
+            $first->visitRoute('posts.search')
+                    ->assertRouteIs('posts.search')
+                    ->type('keyword', $post10->content)
+                    ->assertInputValue('keyword', $post10->content)
+                    ->press('検索する')
+                    ->pause(1000)
+
+                    //投稿本文と投稿ユーザー名と検索ヒットフラッシュメッセージが表示されているか確認
+                    ->assertSee($post10->content)
+                    ->assertSee($this->user->name)
+                    ->assertSee('ヒットしました')
+                    ->screenshot('search');
+
+            //セレクトボックスのみ値を入れて検索
+            $second->visitRoute('posts.search');
+            foreach ($post9->postCategorys as $postCategory) {
+                $second->select('animals_name[]', $postCategory->name)
+                        ->assertSelected('animals_name[]', $postCategory->name);
+            }
+            $second->press('検索する')
+                    ->pause(1000)
+
+                    //投稿本文と投稿ユーザー名と検索ヒットフラッシュメッセージが表示されているか確認
+                    ->assertSee($post9->content)
+                    ->assertSee($this->user->name)
+                    ->assertSee('ヒットしました')
+                    ->screenshot('search');
+
+            //キーワードとセレクトボックス両方値を入れて検索
+            $third->visitRoute('posts.search')
+                    ->type('keyword', $post8->content)
+                    ->assertInputValue('keyword', $post8->content);
+            foreach ($post8->postCategorys as $postCategory) {
+                $third->select('animals_name[]', $postCategory->name)
+                    ->assertSelected('animals_name[]', $postCategory->name);
+            }
+            $third->press('検索する')
+                    ->pause(1000)
+
+                    //投稿本文と投稿ユーザー名と検索ヒットフラッシュメッセージが表示されているか確認
+                    ->assertSee($post8->content)
+                    ->assertSee($this->user->name)
+                    ->assertSee('ヒットしました')
+                    ->screenshot('search');
+        });
+    }
+
+    //投稿検索バリデーションエラー表示テスト
+    public function testValidationPostSearch()
+    {
+        $animals = factory(Animal::class, 15)->make();
+        $this->browse(function (Browser $browser) use ($animals) {
+            $browser->visitRoute('posts.search');
+
+            //セレクトボックスは10個まで選択可能だが、それ以上選択したと仮定
+            foreach ($animals as $animal) {
+                $browser->select('animals_name[]', $animal->name);
+            }
+
+            //バリデーションエラーメッセージと失敗フラッシュメッセージが表示されているか確認
+            $browser->press('検索する')
+                    ->assertSee('検索に失敗しました')
+                    ->assertPresent('.is-invalid')
+                    ->assertPresent('.invalid-feedback')
+                    ->screenshot('search');
+        });
+    }
+
+    //検索データが該当ない時の表示テスト
+    public function testNoPostSearch()
+    {
+        $keyword = '該当無し';
+        $this->browse(function (Browser $browser) use ($keyword) {
+            $browser->visitRoute('posts.search')
+                    ->type('keyword', $keyword)
+                    ->press('検索する')
+
+                    //該当無しのフラッシュメッセージが表示されているか確認
+                    //投稿がないことも確認
+                    ->assertSee('該当する投稿がありません')
+                    ->assertMissing('.post_item')
+                    ->screenshot('search');
+        });
+    }
+}
