43a6426cef3a5dbbdba2a9e8ab2598ede5ab955e
daishi <isitomo.mm@gmail.com>
2020-10-27 22:11:10 +0000

    Fix: masonryのコードにjQuerを使用する修正

--- resources/js/posts_masonry_layout.js
+++ resources/js/posts_masonry_layout.js
@@ -1,36 +1,43 @@
+var $ = require('jquery');
+var jQueryBridget = require('jquery-bridget');
 var Masonry = require('masonry-layout');
 var InfiniteScroll = require('infinite-scroll');
 var imagesLoaded = require('imagesloaded');
+jQueryBridget( 'infiniteScroll', InfiniteScroll, $ );
+jQueryBridget( 'masonry', Masonry, $ );
+imagesLoaded.makeJQueryPlugin( $ );
 
 //トップページ、個別ユーザー投稿一覧、個別ユーザーいいね投稿一覧
 //いいねランキングページ、検索一覧ページ
-document.addEventListener('DOMContentLoaded', function() {
-
-  //投稿自体が存在するかチェック
-  if (document.getElementById('post_card_container') != null) {
-
-    //投稿コンテンツ全体の取得
-    var post_card_container = document.querySelector('#post_card_container');
-
-    //masonryのオプション記述(1つ1つの投稿はまだ取得しない)
-    var msnry = new Masonry( post_card_container, {
-      itemSelector: '.post_item',
+$(function() {
+  if ($('#post_card_container').length) {
+    var $post_card_container = $('#post_card_container').masonry({
+      itemSelector: 'none',
       columnWidth: '.post_sizer',
       percentPosition: true,
       stagger: 30,
-      transitionDuration: '0.7s',
+      transitionDuration: '0.6s'
     });
-
-    //次のページがあれば、無限スクロールの処理に入る
-    if (document.getElementById('pagination_next') != null) {
-      InfiniteScroll.imagesLoaded = imagesLoaded;
-      var infScroll = new InfiniteScroll( post_card_container, {
-        path: '.pagination_next',
-        append: '.post_item',
-        outlayer: msnry,
-        history: false,
-        hideNav: '.pagination',
-        status: '.page_load_status',
+  
+    var msnry = $post_card_container.data('masonry');
+  
+    $post_card_container.imagesLoaded( function() {
+      $post_card_container.removeClass('post_card_container');
+      $post_card_container.masonry( 'option', { itemSelector: '.post_item' });
+      var $items = $post_card_container.find('.post_item');
+      $post_card_container.masonry( 'appended', $items );
+    });
+  
+    InfiniteScroll.imagesLoaded = imagesLoaded;
+  
+    if ($('.pagination_next').length) {
+      $post_card_container.infiniteScroll({
+          path: '.pagination_next',
+          append: '.post_item',
+          outlayer: msnry,
+          history: false,
+          hideNav: '.pagination',
+          status: '.page_load_status',
       });
     }
   }
